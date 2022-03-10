418b603f18a9f70e1ae7ad802cc1751ab9cc8222
Shrenik <101146627+DinoNuggieLover@users.noreply.github.com>
2022-03-10 02:08:19 +0000

    Test Games

--- src/routes/games/[slug].svelte
+++ src/routes/games/[slug].svelte
@@ -6,7 +6,7 @@ export async function load({ params, fetch, session, stuff }) {
     for (let i = 0; i < games.length; i++) {
       let game = games[i];
       if (game.id == params.slug) {
-        game.source = "https://ozh.github.io/cookieclicker/";
+        game.source = "https://eli-schwartz.github.io/cookieclicker/";
         return {
           props: {
             game: game
