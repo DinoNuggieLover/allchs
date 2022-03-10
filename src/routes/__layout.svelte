cbb5deedc77a24a060bf366f1972c50209023425
Shrenik <101146627+DinoNuggieLover@users.noreply.github.com>
2022-03-10 02:00:40 +0000

    Change Footer Size

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -185,7 +185,7 @@
     footer {
         margin-top: auto;
         width: 100%;
-        height: 80px;
+        height: 70px;
         display: flex;
         justify-content: center;
         align-items: center;
